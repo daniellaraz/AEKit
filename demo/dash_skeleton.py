7bc6328eafbed5c87d0682bd5374c9912ab4f79c
Corinne Bintz <corinnebintz@gmail.com>
2019-08-07 15:42:21 -0700

    added text

--- demo/dash_skeleton.py
+++ demo/dash_skeleton.py
@@ -9,10 +9,14 @@ import plotly.graph_objs as go
 app = dash.Dash(__name__)
 
 
+
 app.layout = html.Div([
     html.Div([
         html.H1('Facial Recognition False Positive Demo', id='title'),
-        html.P('Welcome to our facial recognition demo. Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.Welcome to our facial recognition demo.vWelcome to our facial recognition demo.vWelcome to our facial recognition demo.v', id='intro')
+        html.H2('Instructions:', id='instructions'),
+        html.P('Move the slider to change the threshold for the difference between two images that qualifies as a match. If the difference score is  0, the two images are the same. The closer the difference score is to 1 the more different the images are, and the closer the difference score is to 0 the more similar the images are. Matched images are outlined in red.'),
+        html.H3("False Positive:"), html.P("When two images result as a match, but aren't actually the same person, a false positive has occurred. Since Lebron James' image is not in the database of images that we compared his image to, any images that result in a match are a false positive. False positive rates from facial recognition technology are higher for people, especially women, of color. Bias in machine learning algorithms can have adverse social effects.")
+
      ]),
 
     html.Div([
@@ -78,6 +82,7 @@ app.layout = html.Div([
         0.9: '0.9',
         1: '1.0'
     },
+
     )]),
 
     html.Div(
